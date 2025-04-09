559fcfef030554c19e03e593b354da852b32c4af
TheJobShadowers <myla.ariaga-nagl@gmx.at>
2025-04-10 00:04:47 +0200

    test

--- js/benji.js
+++
@@ -1,1 +0,0 @@
-embedYTVideoInTheMainArea("https://www.youtube.com/watch?v=I-yT2Err6PE&t=70s");
--- js/index.js
+++ js/index.js
@@ -7,5 +7,5 @@ setMainContentBackground("#e0f7fa");
 addParagraphToMain("Hi, happy job shadowing.");
 embedYTVideoInTheMainArea("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 
-addImageToAside("https://via.placeholder.com/150", "image!");
+addImageToAside("https://picsum.photos/id/237/200/300", "image!");
 addNoteToAside("Fact: Pizza 🍕 for lunch!");
