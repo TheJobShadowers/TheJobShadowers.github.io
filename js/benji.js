6edce48dc11f5c0237c0570e562c32bc0a3a7aa6
TheJobShadowers <myla.ariaga-nagl@gmx.at>
2025-04-09 23:08:27 +0200

    Update benji.js

--- js/benji.js
+++ js/benji.js
@@ -6,4 +6,4 @@ changeSidebarBackground("lightgray");
 changeSidebarFontColor("darkblue");
 changeSidebarFontSize(18);
 
-extractYouTubeVideoID("https://www.youtube.com/watch?v=I-yT2Err6PE&t=70s");
+embedYTVideoInTheMainArea("https://www.youtube.com/watch?v=I-yT2Err6PE&t=70s");
