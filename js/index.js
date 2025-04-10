a08cee91a3c1bddf9040e3fd8b8917cf7c03da44
TheJobShadowers <myla.ariaga-nagl@gmx.at>
2025-04-10 18:01:38 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,3 +8,4 @@ addParagraphToMain("Hi, happy job shadowing.");
 embedYTVideoInTheMainArea("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 
 AddMiniGame("GuessTheWord", "asideContent1");
+AddMiniGame("MathQuiz", "asideContent2");
